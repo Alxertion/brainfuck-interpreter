79d6f2a64e2e8e0911b0e0d6cce800a00fe4013d
Cosarca Alexandru <cosarca_alex2000@yahoo.com>
2018-08-12 20:37:14 +0300

    -

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,17 +7,27 @@ import memory.Memory16;
 import memory.Memory8;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        Memory memory = new Memory16();
         ClassLoader classLoader = Main.class.getClassLoader();
         File file = new File(classLoader.getResource("testprograms/pi.bf").getFile());
+
+        Memory memory = new Memory16();
         FileInterpreter fileInterpreter = new FileInterpreter(file, memory, "");
         fileInterpreter.run();
+
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+        DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+        formatter.setDecimalFormatSymbols(symbols);
+
         System.out.println();
-        System.out.println("Finished in " + fileInterpreter.getTimeInMilliseconds() + "ms");
-        System.out.println("Finished in " + fileInterpreter.getTimeInSeconds() + "s");
-        System.out.println("Number of executed instructions: " + fileInterpreter.getInstructionCount());
+        System.out.println("Finished in " + fileInterpreter.getTimeInMilliseconds() + "ms (" + fileInterpreter.getTimeInSeconds() + "s)");
+        System.out.println("Number of executed instructions: " + formatter.format(fileInterpreter.getInstructionCount()));
     }
 }
