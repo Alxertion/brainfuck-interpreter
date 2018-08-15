9425bab7540abb2e483e6ca127a666b8a5ca0e42
Cosarca Alexandru <cosarca_alex2000@yahoo.com>
2018-08-15 20:11:39 +0300

    -

--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -1,5 +1,60 @@
 package controller;
 
-public class MainWindowController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
 
+public class MainWindowController {
+    @FXML
+    private TextArea codeTextArea;
+    @FXML
+    private TextArea outputTextArea;
+    @FXML
+    private TextArea inputTextArea;
+
+
+
+    public void newMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void openMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void openExampleMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void exitMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void undoMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void redoMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void cutMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void copyMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void pasteMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void runMenuItemHandler(ActionEvent actionEvent) {
+
+    }
+
+    public void aboutMenuItemHandler(ActionEvent actionEvent) {
+
+    }
 }
