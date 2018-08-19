42e8b9972da56522d8ec1242683703f41a0a4726
Cosarca Alexandru <cosarca_alex2000@yahoo.com>
2018-08-19 12:38:22 +0300

    -

--- src/main/java/controller/MainWindowController.java
+++ src/main/java/controller/MainWindowController.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 
 public class MainWindowController {
     private File currentFile = null;
+    private boolean dirtyFile = false;
     private Stage primaryStage;
 
     @FXML
@@ -23,29 +24,115 @@ public class MainWindowController {
     private TextArea outputTextArea;
     @FXML
     private TextArea inputTextArea;
+    @FXML
+    private String previousContent;
 
     @FXML
     private void initialize() {
-
+        codeTextArea.textProperty().addListener((observableValue, initialContent, newContent) -> {
+            if (!dirtyFile) {
+                previousContent = initialContent;
+                dirtyFile = true;
+                setTitle();
+            } else {
+                if (newContent.equals(previousContent)) {
+                    dirtyFile = false;
+                    setTitle();
+                }
+            }
+        });
     }
 
     @FXML
     private void newMenuItemHandler(ActionEvent actionEvent) {
-
+        //TODO
     }
 
     @FXML
     private void openMenuItemHandler(ActionEvent actionEvent) {
-        FileChooser fileChooser = new FileChooser();
+        FileChooser fileChooser = createFileChooser();
         fileChooser.setTitle("Open File");
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("All Files", "*.*"),
-                new FileChooser.ExtensionFilter("Brainfuck Code", "*.bf", "*.b")
-        );
         File file = fileChooser.showOpenDialog(primaryStage);
         if (file != null) {
             openFile(file);
         }
+    }
+
+    @FXML
+    private void openExampleMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void saveMenuItemHandler(ActionEvent actionEvent) {
+        if (currentFile != null) {
+            saveCodeToFile(currentFile);
+        }
+        else {
+            saveAsMenuItemHandler(null);
+        }
+    }
+
+    @FXML
+    private void saveAsMenuItemHandler(ActionEvent actionEvent) {
+        FileChooser fileChooser = createFileChooser();
+        fileChooser.setTitle("Save File");
+        File file = fileChooser.showSaveDialog(primaryStage);
+        if (file != null) {
+            saveCodeToFile(file);
+        }
+    }
+
+    @FXML
+    private void exitMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void undoMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void redoMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void cutMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void copyMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void pasteMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void runMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void aboutMenuItemHandler(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    private void setTitle() {
+        String title = "";
+        if (dirtyFile) {
+            title = "* " + title;
+        }
+        if (currentFile != null) {
+            title += currentFile.getName() + " - ";
+        }
+        title += ApplicationMain.BRAINFUCK_TITLE;
+        primaryStage.setTitle(title);
     }
 
     private void openFile(File file) {
@@ -54,7 +141,8 @@ public class MainWindowController {
             Files.readLines(file, Charsets.UTF_8).forEach(x -> stringBuilder.append(x).append("\n"));
             codeTextArea.setText(stringBuilder.toString());
             currentFile = file;
-            primaryStage.setTitle(file.getName() + " - " + ApplicationMain.BRAINFUCK_TITLE);
+            dirtyFile = false;
+            setTitle();
         } catch (IOException exception) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Error");
@@ -62,49 +150,29 @@ public class MainWindowController {
         }
     }
 
-    @FXML
-    private void openExampleMenuItemHandler(ActionEvent actionEvent) {
-
+    private void saveCodeToFile(File file) {
+        try {
+            if(!file.getName().contains(".")) {
+                file = new File(file.getAbsolutePath() + ".bf");
+            }
+            Files.write(codeTextArea.getText(), file, Charsets.UTF_8);
+            dirtyFile = false;
+            currentFile = file;
+            setTitle();
+        } catch (IOException exception) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setContentText("File can not be written!");
+        }
     }
 
-    @FXML
-    private void exitMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void undoMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void redoMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void cutMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void copyMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void pasteMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void runMenuItemHandler(ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void aboutMenuItemHandler(ActionEvent actionEvent) {
-
+    private FileChooser createFileChooser() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Files", "*.*"),
+                new FileChooser.ExtensionFilter("Brainfuck Code", "*.bf", "*.b")
+        );
+        return fileChooser;
     }
 
     public void setPrimaryStage(Stage primaryStage) {
